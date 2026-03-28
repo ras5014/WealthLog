f4e806526f3de2fb14b135715e97d67903f6de0d
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2026-03-28 20:59:33 +0530

    Remove server-related files and configurations, including package.json, Prisma setup, routes, controllers, and utility functions, to streamline the project structure and eliminate unused code.

--- server/prisma/migrations/20260131163214_initial_setup/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Accounts" (
-    "id" TEXT NOT NULL,
-    "bankName" TEXT NOT NULL,
-    "accountBalance" DOUBLE PRECISION NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Accounts_pkey" PRIMARY KEY ("id")
-);
--- server/prisma/migrations/20260131184259_dev/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[bankName]` on the table `Accounts` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Accounts_bankName_key" ON "Accounts"("bankName");
