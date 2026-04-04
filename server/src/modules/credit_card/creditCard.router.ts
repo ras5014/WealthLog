bd11b41abdb5ae163d94078f81c61d3db7d99342
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2026-04-05 01:29:47 +0530

    feat: update transaction fetching logic to limit results to the current billing cycle

--- server/src/modules/credit_card/creditCard.router.ts
+++ server/src/modules/credit_card/creditCard.router.ts
@@ -9,7 +9,6 @@ const router = Router();
 const upload = multer({ dest: "uploads" });
 
 router.post("/synchronize", upload.single("file"), process_ICICIStatement);
-// TODO: Only fetch transaction for last 2 months but show only transaction for current billing cycle.
 router.get("/get-transactions", getTransactions);
 
 export default router;
